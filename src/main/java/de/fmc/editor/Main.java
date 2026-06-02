8b46e09c0a7bf2e826f0745cc15b703addcc24ee
Janos Sarkoezi <j.sarkoezi@googlemail.com>
2026-06-03 00:06:43 +0200

    ViewMapper und Java FX hinzufefuegt

---
+++ src/main/java/de/fmc/editor/Main.java
@@ -0,0 +1,45 @@
+package de.fmc.editor;
+
+import de.fmc.editor.core.CoreRegistry;
+import de.fmc.editor.core.factory.FmcFactory;
+import de.fmc.editor.core.model.FmcObject;
+import de.fmc.editor.core.model.FmcType;
+import de.fmc.editor.view.ViewMapper;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // 1. Core Komponenten initialisieren
+        CoreRegistry registry = new CoreRegistry();
+        Pane canvas = new Pane();
+        ViewMapper viewMapper = new ViewMapper(canvas);
+
+        // 2. ViewMapper als Listener registrieren
+        registry.addListener(viewMapper);
+
+        // 3. Test-Daten hinzufügen (Etappe 1 & 2 Demo)
+        java.util.UUID defaultLayer = java.util.UUID.randomUUID();
+        FmcObject circle = FmcFactory.createObject(FmcType.KREIS, 100, 100, defaultLayer);
+        FmcObject square = FmcFactory.createObject(FmcType.QUADRAT, 300, 200, defaultLayer);
+        FmcObject waypoint = FmcFactory.createObject(FmcType.WEGPUNKT, 200, 150, defaultLayer);
+
+        registry.addObject(circle);
+        registry.addObject(square);
+        registry.addObject(waypoint);
+
+        // 4. JavaFX Bühne aufbauen
+        Scene scene = new Scene(canvas, 800, 600);
+        primaryStage.setTitle("FMC Editor - Prototype");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/de/fmc/editor/view/ViewMapper.java
@@ -0,0 +1,62 @@
+package de.fmc.editor.view;
+
+import de.fmc.editor.core.event.RegistryEvent;
+import de.fmc.editor.core.event.RegistryListener;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Shape;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class ViewMapper implements RegistryListener {
+
+    private final Pane canvas; // Das JavaFX Zeichenfenster
+    private final Map<UUID, Shape> visualNodes = new HashMap<>();
+
+    public ViewMapper(Pane canvas) {
+        this.canvas = canvas;
+    }
+
+    @Override
+    public void handleEvent(RegistryEvent event) {
+        // Java 21 Pattern Matching für sealed-Interfaces:
+        switch (event) {
+            case RegistryEvent.ObjectAdded(var obj) -> handleObjectAdded(obj);
+            case RegistryEvent.ObjectRemoved(var id) -> handleObjectRemoved(id);
+            case RegistryEvent.ObjectMoved(var id, var x, var y) -> handleObjectMoved(id, x, y);
+        }
+    }
+
+    private void handleObjectAdded(de.fmc.editor.core.model.FmcObject obj) {
+        Shape shape = switch (obj.type()) {
+            case KREIS -> new Circle(obj.x(), obj.y(), 20);
+            case QUADRAT -> new javafx.scene.shape.Rectangle(obj.x() - 15, obj.y() - 15, 30, 30);
+            case WEGPUNKT -> new Circle(obj.x(), obj.y(), 5);
+        };
+
+        // Wichtig aus den Richtlinien: Nur die ID in den Properties speichern
+        shape.getProperties().put("UUID", obj.id());
+
+        visualNodes.put(obj.id(), shape);
+        canvas.getChildren().add(shape);
+    }
+
+    private void handleObjectMoved(UUID id, double x, double y) {
+        Shape shape = visualNodes.get(id);
+        if (shape instanceof Circle circle) {
+            circle.setCenterX(x);
+            circle.setCenterY(y);
+        } else if (shape instanceof javafx.scene.shape.Rectangle rect) {
+            rect.setX(x - (rect.getWidth() / 2));
+            rect.setY(y - (rect.getHeight() / 2));
+        }
+    }
+
+    private void handleObjectRemoved(UUID id) {
+        Shape shape = visualNodes.remove(id);
+        if (shape != null) {
+            canvas.getChildren().remove(shape);
+        }
+    }
+}
